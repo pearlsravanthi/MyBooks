d0bcc2729d1aad339de18619deac2cb7081194fa
pearlsravanthi <pearlsravanthi@gmail.com>
2017-01-27 09:02:54 +0530

    Initial commit

---
+++ app/src/main/java/app/com/pearlsravanthi/mybooks/BookAdapter.java
@@ -0,0 +1,44 @@
+package app.com.pearlsravanthi.mybooks;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sravanthi on 27/1/17.
+ */
+public class BookAdapter extends ArrayAdapter<Book> {
+    private ArrayList<Book> mBooksList;
+
+    public BookAdapter(Context context, ArrayList<Book> booksList) {
+        super(context, 0, booksList);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Book book = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.book_title);
+        tvTitle.setText(book.getTitle());
+
+        TextView tvSubtitle = (TextView) convertView.findViewById(R.id.subtitle);
+        tvSubtitle.setText(book.getSubtitle());
+
+        TextView tvPublishedDate = (TextView) convertView.findViewById(R.id.published_date);
+        tvPublishedDate.setText(book.getPublishedDate());
+
+        TextView tvAuthors = (TextView) convertView.findViewById(R.id.authors);
+        tvAuthors.setText(book.getAuthors());
+
+        return convertView;
+    }
+}
